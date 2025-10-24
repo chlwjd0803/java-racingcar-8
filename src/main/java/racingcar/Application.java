0945118e1b9c28b1a04469ba2384c8d6366259f8
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-10-24 10:24:56 +0900

    feat : 자동차 정보 입력, 시도횟수 입력
    경주할 자동차들의 정보들과 각 자동차별로 전진 여부를 결정할 횟수를 입력받는 기능을 구현하였습니다.
    
    발견한 예외에서는 이름 글자수를 초과하지 않도록 IllegalArgumentException으로 방지하였으며, 시도할 횟수를 입력받을때도 정수가 아니면 NumberFormatException을 발생시키게 설정하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,45 @@
 package racingcar;
 
+import java.util.HashMap;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static HashMap<String, Integer> carPosition = new HashMap<>();
+    private static Integer tryCount;
+
+    private static void mapping(String name){
+        if(!(name.length() < 5))
+            throw new IllegalArgumentException("이름 글자수가 초과하였습니다.");
+        carPosition.put(name, 0);
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         // 쉼표 기준으로 입력받기, 이름은 5자 이하만 가능
 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] names = input.split(",");
+
+        for(String name : names)
+            mapping(name);
+
 
         // 시도할 횟수 입력받기 (가장 바깥쪽 반복문)
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try{
+            tryCount = Integer.parseInt(Console.readLine());
+        } catch (Exception e){
+            throw new NumberFormatException("숫자가 아닙니다.");
+        }
 
 
-        // 안쪽 반복문은 각 차들을 돌리면 됨
+
+        // 안쪽 반복문은 각 차들의 전진여부를 수정하면 됨
+
 
 
         // 랜덤 0~9에서 4이상의 숫자가 나올경우 전진 else 정지
