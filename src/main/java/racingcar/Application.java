76e0f3e57c2b84e9d47a37fa22a66c4033d5dbd9
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-10-24 09:37:55 +0900

    docs : README 초안 및 주석작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,25 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 쉼표 기준으로 입력받기, 이름은 5자 이하만 가능
+
+
+        // 시도할 횟수 입력받기 (가장 바깥쪽 반복문)
+
+
+        // 안쪽 반복문은 각 차들을 돌리면 됨
+
+
+        // 랜덤 0~9에서 4이상의 숫자가 나올경우 전진 else 정지
+
+
+        // 우승자가 여러명 가능, 쉼표 구분하여 출력
+
+
+        // 잘못된 값을 입력할 경우 IllegalArgumentException 발생
+
+
+
     }
 }
