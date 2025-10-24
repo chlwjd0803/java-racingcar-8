30068af13d388f935d408e2de4d196eae6d7bac8
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-10-24 11:27:50 +0900

    feat : 우승자 출력 기능
    흐름의 마지막 기능인 우승자 출력 기능을 추가하였습니다.
    우승자는 최소 한명이므로 한명은 먼저 출력하고, 공동 우승한 사람이 있다면 연속적으로 출력할 수 있게 구현하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,17 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static HashMap<String, Integer> carPosition = new HashMap<>();
     private static Integer tryCount;
+    private static Integer maxPosition = 0;
+    private static List<String> winners = new ArrayList<>();
 
     private static void mapping(String name){
         if(!(name.length() < 5))
@@ -18,21 +23,39 @@ public class Application {
         // 랜덤 0~9에서 4이상의 숫자가 나올경우 전진 else 정지
         if(Randoms.pickNumberInRange(0, 9) >= 4)
             carPosition.put(name, carPosition.get(name) + 1);
+
+        // maxPosition값 갱신
+        if(carPosition.get(name) > maxPosition)
+            maxPosition = carPosition.get(name);
     }
 
     private static void printTryPosition(String name){
         System.out.print(name + " : ");
+
         for(int i = 0; i < carPosition.get(name); i++)
             System.out.print("-");
+
         System.out.println();
     }
 
+    private static void setWinners(String[] names){
+        for(String name : names){
+            if(carPosition.get(name).equals(maxPosition))
+                winners.add(name);
+        }
+    }
+
+    private static void printWinners(){
+        System.out.print("최종 우승자 : ");
+        System.out.print(winners.get(0));
+        for(int i = 1; i < winners.size(); i++)
+            System.out.print(", " + winners.get(i));
+
+    }
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         // 쉼표 기준으로 입력받기, 이름은 5자 이하만 가능
-
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         String[] names = input.split(",");
@@ -58,13 +81,10 @@ public class Application {
             System.out.println();
         }
 
-
         // 우승자가 여러명 가능, 쉼표 구분하여 출력
-
+        setWinners(names);
+        printWinners();
 
         // 잘못된 값을 입력할 경우 IllegalArgumentException 발생
-
-
-
     }
 }
